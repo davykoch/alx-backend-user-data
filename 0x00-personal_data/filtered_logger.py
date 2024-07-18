d60406e99969bdf73933dbaa345075ad4e5f4970
davykoch <koechdavis24@gmail.com>
2024-07-18 15:30:24 +0300

    Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -58,31 +58,18 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-class DummyCursor:
-    def execute(self, query):
-        pass
-
-    def __iter__(self):
-        return iter([(0,)])
-
-    def close(self):
-        pass
-
-
-def get_db():
-    """Returns connector to the database or a dummy connection if it fails."""
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Returns a connector to the database."""
     username = os.environ.get('PERSONAL_DATA_DB_USERNAME', 'root')
     password = os.environ.get('PERSONAL_DATA_DB_PASSWORD', '')
     host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
     db_name = os.environ.get('PERSONAL_DATA_DB_NAME')
 
-    try:
-        connection = mysql.connector.connect(
-            user=username,
-            password=password,
-            host=host,
-            database=db_name
-        )
-        return connection
-    except mysql.connector.Error:
-        return DummyConnection()
+    connection = mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=db_name
+    )
+
+    return connection
