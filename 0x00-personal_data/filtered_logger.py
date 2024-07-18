94f8a969390cc2578c8cafd6acc03dde00db2b58
davykoch <koechdavis24@gmail.com>
2024-07-18 15:23:03 +0300

    Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -58,7 +58,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db() -> mysql.connector.connection.MySQLConnection:
+def get_db():
     """Returns a connector to the database."""
     username = os.environ.get('PERSONAL_DATA_DB_USERNAME', 'root')
     password = os.environ.get('PERSONAL_DATA_DB_PASSWORD', '')
@@ -74,5 +74,4 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         )
         return connection
     except mysql.connector.Error as err:
-        print(f"Error connecting to the database: {err}", file=sys.stderr)
         return None
