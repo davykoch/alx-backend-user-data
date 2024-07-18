01f9721b9bd2c5ad4dcd74becb334d4a2fc60efa
davykoch <koechdavis24@gmail.com>
2024-07-19 02:38:00 +0300

    Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""BasicAuth class for the API authentication"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth class to manage API basic authentication"""
+    pass
