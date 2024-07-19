04c02b6cb6305d489f03db68186195a35de4b4d1
davykoch <koechdavis24@gmail.com>
2024-07-19 22:49:00 +0300

    New view for Session Authentication

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -15,7 +15,7 @@ import os
 def login():
     """
     Handles user login and session creation
-    
+
     Returns:
         - JSON representation of the user if login is successful
         - Error message with appropriate status code if login fails
@@ -23,19 +23,19 @@ def login():
     email = request.form.get('email')
     if not email:
         return jsonify({"error": "email missing"}), 400
-    
+
     password = request.form.get('password')
     if not password:
         return jsonify({"error": "password missing"}), 400
-    
+
     users = User.search({'email': email})
     if not users:
         return jsonify({"error": "no user found for this email"}), 404
-    
+
     user = users[0]
     if not user.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
-    
+
     session_id = auth.create_session(user.id)
     response = jsonify(user.to_json())
     session_name = os.getenv('SESSION_NAME')
