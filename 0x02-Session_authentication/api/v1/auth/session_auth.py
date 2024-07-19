9fd72c6e7251d5a9732955c3531f726972bd6b9d
davykoch <koechdavis24@gmail.com>
2024-07-19 14:44:02 +0300

    Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""SessionAuth module for the API"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """SessionAuth class to manage API session authentication"""
+    pass
